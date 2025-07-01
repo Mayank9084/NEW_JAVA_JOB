7abe9fbd918753a2e4b30685f8854eae8cb49dfd
Mayank Singh <120007455+Mayank9084@users.noreply.github.com>
2025-07-01 18:17:20 +0530

    Three Number Sum

---
+++ Sum_three_No.java
@@ -0,0 +1,11 @@
+public class Sum_three_No {
+    public static void main(String[] args) {
+        // Add three numbers
+        int a = 3;
+        int b = 4;
+        int c = 5;
+        int sum = a + b + c;
+        System.out.println("Sum of three numbers : ");
+        System.out.println(sum);
+    }
+}
