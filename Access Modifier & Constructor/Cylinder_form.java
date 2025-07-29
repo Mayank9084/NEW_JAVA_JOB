809e776bab58b5c3eaffa67d59135f21a9258db4
Mayank Singh <120007455+Mayank9084@users.noreply.github.com>
2025-07-29 12:09:58 +0530

    Surface Area and volume using Getter and Setter

--- Access Modifier & Constructor/Cylinder_form.java
+++ Access Modifier & Constructor/Cylinder_form.java
@@ -1,31 +1,45 @@
 package Access_modifiers_Comstructor;
 
-class Cylinder{
-    private int radius;
-    private int height;
+import java.util.Scanner;
 
-    public int getRadius() {
+class Cylinder{
+    private float radius;
+    private float height;
+    private float pie = 3.14159f;
+
+    public float getRadius() {
         return radius;
     }
 
-    public void setRadius(int radius) {
+    public void setRadius(float radius) {
         this.radius = radius;
     }
 
-    public int getHeight() {
+    public float getHeight() {
         return height;
     }
 
-    public void setHeight(int height) {
+    public void setHeight(float height) {
         this.height = height;
+    }
+    public float surfaceArea(){
+        return  2*pie*radius*radius + 2*pie*radius*height;
+    }
+    public float volume(){
+        return  pie*radius*radius*height;
     }
 }
 public class Cylinder_form {
     public static void main(String[] args) {
         Cylinder mayank = new Cylinder();
-        mayank.setHeight(4);
-        System.out.println("Height of CYLINDER is " + mayank.getHeight());
-        mayank.setRadius(9);
-        System.out.println("Radius of CYLINDER is " + mayank.getRadius() );
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Height of Cylinder : ");
+        mayank.setHeight(sc.nextFloat());
+        System.out.print("Height of CYLINDER is " + mayank.getHeight());
+        System.out.print("\nEnter Radius of Cylinder : ");
+        mayank.setRadius(sc.nextFloat());
+        System.out.print("Radius of CYLINDER is " + mayank.getRadius() );
+        System.out.println("\nSurface Area = " + mayank.surfaceArea());
+        System.out.println("\nVolume = " + mayank.volume());
     }
 }
