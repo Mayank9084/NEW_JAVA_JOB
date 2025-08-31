a659a38522c6433f7cbcef38c893055961393206
Mayank Singh <120007455+Mayank9084@users.noreply.github.com>
2025-08-31 10:33:42 +0530

    how to use it
    try (
    
        // code
    
        )
    
       Catch (To Exception e) - Handle all Exceptions of type IO Exception
    
            // code
    
            )
    
       Catch (Exception e) - Handle all Exceptions of type Arithmetic Exception
    
            // code
    
             ) 
    
       Catch (Exception e) - Handle all other Exceptions
    
            // code
    
            )

---
+++ Error_Exception/Specific_Exception.java
@@ -0,0 +1,31 @@
+package Error_Exception;
+
+import java.util.Scanner;
+
+public class Specific_Exception {
+    public static void main(String[] args) {
+        int [] marks = new int[3];
+        marks[0] = 7;
+        marks[1] = 56;
+        marks[2] = 6;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Array Index ");
+        int ind = sc.nextInt();
+        System.out.print("Enter the number you want to divide the value with ");
+        int number = sc.nextInt();
+        try {
+            System.out.println("The value at array index entered is " + marks[ind]);
+            System.out.println("The value of array-value/number is " + marks[ind]/number);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Arithmetic Exception Occurred ");
+            System.out.println(e);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexOutOfBounds Exception Occurred ");
+            System.out.println(e);
+        } catch (Exception e){
+            System.out.println("Some Exception Occurred ");
+            System.out.println(e);
+        }
+    }
+}
