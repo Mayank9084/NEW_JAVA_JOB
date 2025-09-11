618f07d1ef7fc4e2f7b942a5e988adb91994ee19
Mayank Singh <120007455+Mayank9084@users.noreply.github.com>
2025-09-11 12:18:29 +0530

    Add files via upload
    Date Class In Java :
    Date class in java is available in java.util package.
    This class provides the instant in time with precision of millisecond.
    Constructors of the date class :
    Date(): This constructor is used when we need an object of current date and time.
    Date(long milliseconds): This constructor creates a date object from the number of milliseconds passed since January 1, 1970.
    Example :
    import java.util.*;
    public class CWH extends Thread{
        public static void main(String[] args) {
            Date d= new Date();
            System.out.println("The current date is : " + d);
    
            Date d1= new Date(1621709639111l);
            System.out.println("The date calculated form miliseconds is : " + d1);
    
        }
    }
    Output :
    The current date is : Sun May 23 00:24:17 IST 2021
    The date calculated form miliseconds is : Sun May 23 00:23:59 IST 2021
    Methods of date class :
    compareTo() : 
    Checks for the equality of the two dates.
    Returns 0 if the dates are equal; else, returns 1.
    Example :
    import java.util.*;
    public class CWH extends Thread{
        public static void main(String[] args) {
            Date d= new Date();
    
            Date d1= new Date(2021,12, 24);  //both dates are different
            System.out.println(d1.compareTo(d));
            
        }
    }
    Output :
    1
    getTime() : 
    This method returns the number of milliseconds passed since the midnight of January 1, 1970.
    Example :
    import java.util.*;
    public class CWH extends Thread{
        public static void main(String[] args) {
            Date d= new Date(2021,5,23);
    
            System.out.println("The number of milliseonds passed since Jan 1, 1970 :" +d.getTime() );
    
        }
    }
    Output :
    The number of milliseonds passed since Jan 1, 1970 :61582530600000
     getYear() : Prints the current year.
    getDate() : Prints the current date.
    Example :
    import java.util.*;
    public class CWH extends Thread{
        public static void main(String[] args) {
            Date d= new Date();
            System.out.println("The current date is : "+ d.getDate());
            System.out.println("The current year is : "+ d.getYear()); //19
    
        }
    }
    Output :
    The current date is : 23
    The current year is : 121

--- Advance_JAVA/Date_Time.java
+++ Advance_JAVA/Date_Time.java
@@ -1,9 +1,15 @@
 package Advance_Java;
-
+import java.util.Calendar;
+import java.util.Date;
 public class Date_Time {
     public static void main(String[] args) {
-        //Millisec 1970 se count krta hai year
-        System.out.println(System.currentTimeMillis()/1000/3600/24/365);
-        
+      Date d = new Date();
+        System.out.println(d);
+        System.out.println(d.getTime());
+        // Setting a custom date using Calendar
+        Calendar cal = Calendar.getInstance();
+        cal.set(2030, Calendar.JANUARY, 1); // Year, Month (0-based), Day
+        Date date = cal.getTime();
+        System.out.println(d.after(date));
     }
 }
