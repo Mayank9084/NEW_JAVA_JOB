e6fd9ed316fd793e92660532c5b17b210a97ae37
Mayank Singh <120007455+Mayank9084@users.noreply.github.com>
2025-09-11 11:56:33 +0530

    Add files via upload
    Date & Time in Java
    java time -> package for date & time in java from java onwards
    
    Before java 8, java util package used to hold the date time class now these classes are deprecated 
    
    How java stores a Date?
    Date in java is stored in the form of a long numer. This long number holds the number of miliseconds passed since 1 jan 1970
    
    Java assumes that 1900 is the start year which means it calculates years passed since 1900 whenever We ask it for years passed 
    
    System current Time Millis () returns no of sound passed Once no. of ms are calculated, we can calculate minutes, seconds & years passed

---
+++ Advance_JAVA/Date_Time.java
@@ -0,0 +1,9 @@
+package Advance_Java;
+
+public class Date_Time {
+    public static void main(String[] args) {
+        //Millisec 1970 se count krta hai year
+        System.out.println(System.currentTimeMillis()/1000/3600/24/365);
+        
+    }
+}
